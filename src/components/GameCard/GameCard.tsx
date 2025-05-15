2a20126c3c99bfec3fc1ea4f6085c784e77e0a92
Adam Fenwick <afenwickdev@gmail.com>
2025-05-15 14:10:42 +0100

    Updating tsconfig, vitest and gamecard tests

--- src/components/GameCard/GameCard.tsx
+++ src/components/GameCard/GameCard.tsx
@@ -1,22 +1,24 @@
-import { Link } from 'react-router';
-import './GameCard.css';
-import { format } from 'date-fns';
+import { Link } from "react-router";
+import "./GameCard.css";
+import { format } from "date-fns";
 
 const GameCard = ({ game }) => {
-  const formattedDate = format(new Date(game.released), 'dd MMMM yyyy');
+  const formattedDate = format(new Date(game.released), "dd MMMM yyyy");
 
   return (
-    <li className="game-list-item">
+    <li data-testid="game-card" className="game-list-item">
       <img
         src={game.background_image}
         alt={`${game.name} cover image`}
         className="game-list-item-cover-art"
       />
-      <span className="game-list-item-rating">{game.metacritic}</span>
-      <p>
+      <span data-testid="game-metacritic" className="game-list-item-rating">
+        {game.metacritic}
+      </span>
+      <p data-testid="game-title">
         <Link to={`/game/${game.id}`}>{game.name}</Link>
       </p>
-      <p>{formattedDate}</p>
+      <p data-testid="release-date">{formattedDate}</p>
       <ul className="game-list-item-features">
         <li>Feature</li>
         <li>Feature</li>
