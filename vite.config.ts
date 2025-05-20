c48fc18cebe0bd3ae6f916c83b938c9e33ba2ece
Adam Fenwick <afenwickdev@gmail.com>
2025-05-20 12:33:24 +0100

    Updating to add test envs

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  test: {
-    coverage: {
-      provider: 'v8'
-    }
-  }
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react()],
+    test: {
+      coverage: {
+        provider: "v8",
+      },
+    },
+    define: {
+      "import.meta.env": {
+        ...env,
+      },
+    },
+  };
+});
