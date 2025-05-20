d62c64a36d5ebdc2733abae7a16d939093803aee
Adam Fenwick <afenwickdev@gmail.com>
2025-05-20 12:50:57 +0100

    Updating env in workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,12 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-
-  return {
-    plugins: [react()],
-    test: {
-      coverage: {
-        provider: "v8",
-      },
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    coverage: {
+      provider: "v8",
     },
-    define: {
-      "import.meta.env": {
-        ...env,
-      },
-    },
-  };
+  },
 });
